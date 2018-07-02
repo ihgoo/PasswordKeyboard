f99d98ba99a710dba47657ceaa529d9793eaf358
ihgoo <ihgoo2008@gmail.com>
2018-07-02 18:02:11 +0800

    add password method

--- password_keyboard/src/main/java/project/com/password_keyboard/adapter/AdapterPasswordKeybord.java
+++ password_keyboard/src/main/java/project/com/password_keyboard/adapter/AdapterPasswordKeybord.java
@@ -6,9 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import project.com.password_keyboard.R;
+import project.com.password_keyboard.linstener.OnKeybordClickListener;
+import project.com.password_keyboard.linstener.OnPasswordCallBack;
 
 /**
  * Created by ihgoo
@@ -16,6 +19,42 @@ import project.com.password_keyboard.R;
 public class AdapterPasswordKeybord extends RecyclerView.Adapter<AdapterPasswordKeybord.ViewHolder> {
 
     private final int KEYBORD_SIZE = 12;
+    private OnKeybordClickListener mOnKeybordClickListener;
+    private OnPasswordCallBack mOnPasswordCallBack;
+    private int passwordLength;
+    private StringBuffer password;
+    private int nowPasswordLength;
+
+    public AdapterPasswordKeybord() {
+        init();
+    }
+
+    private void init() {
+        password = new StringBuffer();
+        passwordLength = 6;
+    }
+
+
+    public int getPasswordLength() {
+        return passwordLength;
+    }
+
+    public void setPasswordLength(int passwordLength) {
+        this.passwordLength = passwordLength;
+    }
+
+    public void setOnKeybordClickListener(OnKeybordClickListener onKeybordClickListener) {
+        this.mOnKeybordClickListener = onKeybordClickListener;
+    }
+
+    public OnPasswordCallBack getOnPasswordCallBack() {
+        return mOnPasswordCallBack;
+    }
+
+    public void setOnPasswordCallBack(OnPasswordCallBack onPasswordCallBack) {
+        this.mOnPasswordCallBack = onPasswordCallBack;
+    }
+
 
     @NonNull
     @Override
@@ -26,23 +65,66 @@ public class AdapterPasswordKeybord extends RecyclerView.Adapter<AdapterPassword
     }
 
     @Override
-    public void onBindViewHolder(AdapterPasswordKeybord.ViewHolder holder, int position) {
-        int key = position+1;
+    public void onBindViewHolder(AdapterPasswordKeybord.ViewHolder holder, final int position) {
+        int key = position + 1;
+
 
         if (position < 9) {
             holder.tvKey.setText(String.valueOf(key));
         } else if (position == 9) {
-            holder.tvKey.setText(".");
+            holder.tvKey.setText("");
         } else if (position == 10) {
             holder.tvKey.setText("0");
         }
 
-        if (position < 11) {
+        if (position < 11 && position != 9) {
             holder.viewStub.setVisibility(View.GONE);
             holder.tvKey.setVisibility(View.VISIBLE);
+        } else if (position == 9) {
+            holder.viewStub.setVisibility(View.GONE);
+            holder.tvKey.setVisibility(View.GONE);
         } else {
             holder.tvKey.setVisibility(View.GONE);
             holder.viewStub.setVisibility(View.VISIBLE);
+        }
+
+
+        holder.itemKeybord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (position < 9) {
+                    mOnKeybordClickListener.onNumberClick(key);
+                    passwordApped(key);
+                } else if (position == 9) {
+
+                } else if (position == 10) {
+                    mOnKeybordClickListener.onNumberClick(0);
+                    passwordApped(key);
+                } else if (position == 11) {
+                    passwordSub();
+                    mOnKeybordClickListener.onDeleteClick();
+                }
+
+                if (nowPasswordLength == passwordLength) {
+                    mOnPasswordCallBack.wholePassword(password.toString());
+                }
+
+            }
+        });
+
+    }
+
+    private void passwordSub() {
+        if (nowPasswordLength > 0) {
+            nowPasswordLength--;
+            password.delete(password.length() - 1, password.length());
+        }
+    }
+
+    private void passwordApped(int key) {
+        if (password.length() < passwordLength) {
+            password.append(String.valueOf(key));
+            nowPasswordLength++;
         }
     }
 
@@ -54,10 +136,12 @@ public class AdapterPasswordKeybord extends RecyclerView.Adapter<AdapterPassword
     class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvKey;
         ViewStub viewStub;
+        RelativeLayout itemKeybord;
 
         ViewHolder(View itemView) {
             super(itemView);
             tvKey = itemView.findViewById(R.id.tv_keys);
+            itemKeybord = itemView.findViewById(R.id.item_keybord);
             viewStub = itemView.findViewById(R.id.view_stub);
         }
     }
