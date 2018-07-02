d5eb0c80a6c43032eb3bf923f2413528c9071bd1
ihgoo <ihgoo2008@gmail.com>
2018-07-02 17:59:07 +0800

    add interfaces

---
+++ password_keyboard/src/main/java/project/com/password_keyboard/linstener/OnKeybordClickListener.java
@@ -0,0 +1,10 @@
+package project.com.password_keyboard.linstener;
+
+/**
+ * Created by ihgoo
+ */
+public interface OnKeybordClickListener {
+    void onNumberClick(int number);
+
+    void onDeleteClick();
+}
---
+++ password_keyboard/src/main/java/project/com/password_keyboard/linstener/OnPasswordCallBack.java
@@ -0,0 +1,8 @@
+package project.com.password_keyboard.linstener;
+
+/**
+ * Created by ihgoo
+ */
+public interface OnPasswordCallBack {
+    void wholePassword(String password);
+}
