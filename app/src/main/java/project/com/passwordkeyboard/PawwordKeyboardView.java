4822dbd11702259dab7cfba0f35e3f9e383263ac
ihgoo <ihgoo2008@gmail.com>
2018-07-02 09:52:03 +0800

    init

---
+++ app/src/main/java/project/com/passwordkeyboard/PawwordKeyboardView.java
@@ -0,0 +1,18 @@
+package project.com.passwordkeyboard;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by ihgoo
+ */
+public class PawwordKeyboardView extends RelativeLayout {
+
+    public PawwordKeyboardView(Context context) {
+        super(context);
+    }
+
+
+
+
+}
